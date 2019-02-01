8c2dd3e1c8abe781cc5c5b8135e06fe1879a9e16
Jimmy Svensson <jimmy888swe@gmail.com>
2019-02-01 13:53:08 +0100

    fullNameWorking&ChangeIfErrToThrow

--- fullName.js
+++ fullName.js
@@ -1,5 +1,7 @@
-const fs = require('fs');
 
+// deklarerar sökvägar mm
+
+const fs = require('fs');
 var pathOne = './user_Svensson.txt';
 var pathTwo = './user_Jimmy.txt';
 var path = `./user_full.txt`;
@@ -7,27 +9,21 @@ var options = {
     encoding: 'utf8',
     flag: 'w+'
 };
-fs.readFile(pathOne, (err, data) =>{
-    if (err){
+
+// läser in efternamn
+fs.readFile(pathOne, (err, dataOne) =>{
+    if (err) throw err;
         console.error(err)
-    }
-    else {
-        console.log(`
-        Name: ${data}
-        
-        `)
-    }
+// läser in förnamn   
+    fs.readFile(pathTwo, (err, dataTwo) =>{
+        if (err) throw err;
+            console.error(err)
+//  sätter ihop namnet och skriver ut i full.txt
+        let fullName = dataTwo +' ' + dataOne;
+        fs.writeFile(path, fullName, options, (err) =>{
+            if (err) throw err;
+            console.log('Dokumentet klar!');
+    
+        })
+    })  
 })
-
-fs.readFile(pathTwo, (err, dataTwo) =>{
-    if (err){
-        console.error(err)
-    }
-    else {
-        console.log(`
-        Name: ${dataTwo}
-        `)
-    }
-})
-
-
