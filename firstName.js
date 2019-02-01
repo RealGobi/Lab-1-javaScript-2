02b754379549d19bc86638c5cec3c194a307bb62
Jimmy Svensson <jimmy888swe@gmail.com>
2019-02-01 13:58:12 +0100

    finalCheck

--- firstName.js
+++ firstName.js
@@ -1,15 +1,14 @@
+// deklarera sökväg, options.
+
 const fs = require('fs');
-
 var firstName = 'Jimmy'
-
 var path = `./user_${firstName}.txt`;
-
-
 var options = {
     encoding: 'utf8',
     flag: 'w+'
 };
 
+// skriver filen med förnamnet
 fs.writeFile(path, firstName, options, (err) =>{
     if (err){
         console.error(err);
--- lastName.js
+++ lastName.js
@@ -1,14 +1,14 @@
+// deklarera sökväg, options.
+
 const fs = require('fs');
-
 var lastname = 'Svensson'
-
 var path = `./user_${lastname}.txt`;
-
 var options = {
     encoding: 'utf8',
     flag: 'w+'
 };
 
+// skriver filen med efternamnet
 fs.writeFile(path, lastname, options, (err) =>{
     if (err){
         console.error(err);
